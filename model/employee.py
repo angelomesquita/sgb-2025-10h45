e21c5799f41bd01003cd4581ffa80eadc06f75db
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-05-28 11:25:49 -0300

    RF001: alterando no atributo cargo (job_position) para role

--- model/employee.py
+++ model/employee.py
@@ -1,10 +1,13 @@
 class Employee:
-    def __init__(self, name, cpf, job_position, login, password):
+
+    # TODO: apply encapsulation by converting attributes to private or protected instead of public
+
+    def __init__(self, name, cpf, role, login, password_hash):
         self.name = name
         self.cpf = cpf
-        self.job_position = job_position
+        self.role = role
         self.login = login
-        self.password = password
+        self.password_hash = password_hash
 
     def __str__(self):
-        return f'Name: {self.name}, CPF: {self.cpf}, Job Position: {self.job_position}, Login: {self.login}'
+        return f'Name: {self.name}, CPF: {self.cpf}, Role: {self.role}, Login: {self.login}'
