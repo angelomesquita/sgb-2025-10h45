3f21c638314585008ffc309005e74f1fe1761a3d
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-05-28 12:06:43 -0300

    RF001: atualizando atributo login para username

--- model/employee.py
+++ model/employee.py
@@ -2,12 +2,12 @@ class Employee:
 
     # TODO: apply encapsulation by converting attributes to private or protected instead of public
 
-    def __init__(self, name, cpf, role, login, password_hash):
+    def __init__(self, name, cpf, role, username, password_hash):
         self.name = name
         self.cpf = cpf
         self.role = role
-        self.login = login
+        self.username = username
         self.password_hash = password_hash
 
     def __str__(self):
-        return f'Name: {self.name}, CPF: {self.cpf}, Role: {self.role}, Login: {self.login}'
+        return f'Name: {self.name}, CPF: {self.cpf}, Role: {self.role}, Username: {self.username}'
