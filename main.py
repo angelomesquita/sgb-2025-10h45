31aae4d548c898e0d2a7e0bafa59fc7537a8291c
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-05-28 12:07:37 -0300

    RF001: implemtando módulo de funcionarios

---
+++ main.py
@@ -0,0 +1,27 @@
+from controller.employee_controller import EmployeeController
+from view.employee_view import *
+
+
+def main():
+    controller = EmployeeController()
+
+    while True:
+        show_menu()
+        option = input('Select an option: ') # Escolha uma opção
+
+        if option == '1':
+            register_employee(controller)
+        elif option == '2':
+            list_employees(controller)
+        elif option == '3':
+            authenticate_employee(controller)
+        elif option == '0':
+            print('Exiting the system...') # Saindo do sistema
+            break
+        else:
+            print('Invalid option.') # Opção inválida
+            press_enter_to_continue()
+
+
+if __name__ == '__main__':
+    main()
