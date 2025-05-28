c1ab9ac9f5aeb0be20d7cb82d15483d4447d3e4d
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-05-28 11:26:37 -0300

    RF001: implementando métodos no employee_controller
    - register(): para persistir um novo funcionario
    - list(): para exibir uma lista com todos os funcionarios
    cadastrados

--- controller/employee_controller.py
+++ controller/employee_controller.py
@@ -1,14 +1,20 @@
 from model.employee import Employee
+from model.auth import Auth
 
 
 class EmployeeController:
     def __init__(self):
         self.employees = []
 
-    def save(self, name, cpf, job_position, login, password):
-        employee = Employee(name, cpf, job_position, login, password)
+    def register(self, name, cpf, role, login, password):
+        password_hash = Auth.hash_password(password)
+        employee = Employee(name, cpf, role, login, password_hash)
         self.employees.append(employee)
         print('✅ Employee successfully registered!\n')
 
-    def all(self):
-        return self.employees
+    def list(self):
+        if not self.employees:
+            print("No employees registered yet.")
+            return
+        for employee in self.employees:
+            print(employee)
