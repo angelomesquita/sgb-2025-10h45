5636a2d65a33b1b8320724d8f8887d2a67f3e57e
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-06-11 12:09:51 -0300

    RF002: criando a customer_view

--- view/customer_view.py
+++ view/customer_view.py
@@ -1,90 +1,39 @@
-import os
-from controller.employee_controller import EmployeeController
-from controller.auth_controller import AuthController
+from view.view import View
+from controller.customer_controller import CustomerController
 from typing import Tuple
 from getpass import getpass
 
 
-def show_menu() -> None:
-    clear_screen()
-    print('\n=== Library Management System ===')  # Sistema Gerenciador de Bibliotecas
-    print('1. Register Employee ')  # Cadastrar Funcionario
-    print('2. List Employees')  # Listar Funcionarios
-    print('3. Authenticate Employee')  # Autenticar Funcionario
-    print('4. Update Employee')  # Atualizar Funcionario
-    print('5. Delete Employee')  # Apagar Funcionario
-    print('0. Exit')  # Sair
+class CustomerView(View):
 
+    def show_menu(self) -> None:
+        self.clear_screen()
+        print('\n=== Customer Module ===')  # Módulo de Usuário
+        print('1. Register Customer ')  # Cadastrar Usuário (Estudante, Professor ou Visitante)
+        #print('2. List Employees')  # Listar Funcionarios
+        #print('3. Authenticate Employee')  # Autenticar Funcionario
+        #print('4. Update Employee')  # Atualizar Funcionario
+        #print('5. Delete Employee')  # Apagar Funcionario
+        #print('0. Exit')  # Sair
 
-def register_employee(controller: EmployeeController) -> None:
-    print('\n=== Register Employee ===')
-    data = get_employee_data()
-    controller.register(*data)
-    press_enter_to_continue()
+    def register_employee(self, controller: CustomerController) -> None:
+        print('\n=== Register Employee ===')
+        data = self.get_customer_data()
+        controller.register(*data)
+        self.press_enter_to_continue()
 
+    def get_customer_data(self) -> Tuple[str, str, str, str, str]:
+        name = input('Name: ')
+        cpf = self.get_cpf_customer()
+        role = input('Role: ')
+        login, password = self.get_auth_data()
+        return name, cpf, role, login, password
 
-def list_employees(controller: EmployeeController) -> None:
-    print('\n=== List Employees ===')
-    controller.list()
-    press_enter_to_continue()
+    def get_auth_data(self) -> Tuple[str, str]:
+        username = input('Username: ')
+        password = getpass('Password: ')
+        return username, password
 
-
-def update_employee(controller: EmployeeController) -> None:
-    print('\n=== Update Employee ===')
-    cpf = get_cpf_employee()
-    employee = controller.find(cpf)
-    if employee:
-        data = get_employee_data()
-        controller.update(*data)
-    else:
-        print(employee_not_found())
-    press_enter_to_continue()
-
-
-def delete_employee(controller: EmployeeController) -> None:
-    print('\n=== Delete Employee ===')
-    cpf = get_cpf_employee()
-    employee = controller.find(cpf)
-    if employee:
-        controller.delete(cpf)
-    else:
-        print(employee_not_found())
-    press_enter_to_continue()
-
-
-def authenticate_employee(controller: EmployeeController) -> None:
-    print('\n=== Authenticate Employee ===')
-    username, password = get_auth_data()
-    AuthController.auth(controller.employees, username, password)
-    press_enter_to_continue()
-
-
-def get_employee_data() -> Tuple[str, str, str, str, str]:
-    name = input('Name: ')
-    cpf = get_cpf_employee()
-    role = input('Role: ')
-    login, password = get_auth_data()
-    return name, cpf, role, login, password
-
-
-def get_auth_data() -> Tuple[str, str]:
-    username = input('Username: ')
-    password = getpass('Password: ')
-    return username, password
-
-
-def get_cpf_employee() -> str:
-    cpf = input('CPF: ')
-    return cpf
-
-
-def clear_screen() -> None:
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-
-def press_enter_to_continue() -> None:
-    input('Press Enter to continue...')
-
-
-def employee_not_found() -> str:
-    return 'Employee not found!\n'
+    def get_cpf_customer(self) -> str:
+        cpf = input('CPF: ')
+        return cpf
