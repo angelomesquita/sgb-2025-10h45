bf1f8fa6aaab4014c4d7b08e3b9c8fb137f1563e
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-06-11 11:04:14 -0300

    RF002: criando soft delete

--- model/customer.py
+++ model/customer.py
@@ -1,12 +1,18 @@
+from model.category import Category
+
+
 class Customer:
 
     # TODO: apply encapsulation by converting attributes to private or protected instead of public
 
-    def __init__(self, name: str, cpf: str, contact: str, category: str):
+    def __init__(self, name: str, cpf: str, contact: str, category: str, password_hash: str):
+        # TODO Is Customer equals to Employee???
         self.name = name
         self.cpf = cpf
         self.contact = contact
-        self.category = category # student, teacher, visitor
+        self.category = category  # student, teacher, visitor
+        self.password_hash = password_hash
+        self.deleted = False
 
     def __str__(self):
         return f'Name: {self.name}, CPF: {self.cpf}, Contact: {self.contact}, Category: {self.category}'
