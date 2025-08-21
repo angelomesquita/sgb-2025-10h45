9eeab416c8e7404a3d848afbe016e1f366696f87
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-08-21 08:18:52 -0300

    add logs em auth_controller

--- controller/auth_controller.py
+++ controller/auth_controller.py
@@ -1,4 +1,5 @@
 from model.auth import Auth
+from model.logger import auth_logger
 
 
 class AuthController:
@@ -8,6 +9,8 @@ class AuthController:
         for employee in employees:
             if Auth.auth(employee, username, password) and employee.deleted is not True:
                 print(f'Welcome, {employee.name}')
+                auth_logger.debug(f"Login attempt successfully for employee: {employee.username} ({employee.name})")
                 return True
         print('Authentication failed.')
+        auth_logger.debug(f"Login attempt failed for employee: {employee.username} ({employee.name})")
         return False
