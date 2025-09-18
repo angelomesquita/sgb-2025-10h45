cc4a61566dcd905b775773efbf5f6edf9906d125
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-09-18 07:47:44 -0300

    add author controller

---
+++ controller/author_controller.py
@@ -0,0 +1,33 @@
+from controller.base_controller import BaseController
+from model.author import Author
+from model.author_dao import AuthorDao
+from model.logger import author_logger
+from model.exceptions import (
+    AuthorAlreadyExistsError,
+    AuthorDeletedError,
+    AuthorNotFoundError,
+    AuthorLoadError
+)
+
+
+class AuthorController(BaseController[Author]):
+
+    dao_class = AuthorDao
+    logger = author_logger
+
+    AlreadyExistsError = AuthorAlreadyExistsError
+    DeleteExistsError = AuthorDeletedError
+    NotFoundError = AuthorNotFoundError
+    LoadError = AuthorLoadError
+
+    def __init__(self):
+        super().__init__(model_class=Author, key_field="author_id")
+
+    def register(self, author_id: int, name: str) -> None:
+        super().register(author_id, name=name)
+
+    def create_instance(self, author_id: int, name: str, deleted: bool = False) -> Author:
+        return Author(author_id, name, deleted)
+
+    def update(self, author_id: int, name: str) -> None:
+        super().update(author_id, name=name)
