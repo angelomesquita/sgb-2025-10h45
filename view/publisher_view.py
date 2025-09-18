0b7a9534e103201b2c0303130fe9bcb72aeaa73a
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-09-18 08:46:36 -0300

    add publisher view

---
+++ view/publisher_view.py
@@ -0,0 +1,114 @@
+from typing import Tuple
+
+from controller.publisher_controller import PublisherController
+from validators.publisher_validator import PublisherValidator
+from view.view import View
+
+
+class PublisherView(View):
+
+    __NOT_FOUND = 'Publisher not found!\n'
+
+    def __init__(self):
+        self.controller = PublisherController()
+
+    def show_menu(self) -> None:
+        while True:
+            self.clear_screen()
+            print('\n=== Publisher Module ===')
+            print('1. Register Publisher ')
+            print('2. List Publishers ')
+            print('3. Update Publisher')
+            print('4. Delete Author')
+            print('0. Back to main menu')
+
+            option = input('Select an option: ')
+
+            if option == '1':
+                self.register()
+            elif option == '2':
+                self.list()
+            elif option == '3':
+                self.update()
+            elif option == '4':
+                self.delete()
+            elif option == '0':
+                break
+            else:
+                print(View._MENU_INVALID_OPTION)
+                self.press_enter_to_continue()
+
+    def register(self) -> None:
+        print('\n=== Register Publisher ===')
+        data = self.get_publisher_data()
+        self.controller.register(*data)
+        self.press_enter_to_continue()
+        self.clear_screen()
+
+    def list(self) -> None:
+        print('\n=== List Publishers ===')
+        self.controller.list()
+        self.press_enter_to_continue()
+        self.clear_screen()
+
+    def update(self) -> None:
+        print('\n=== Update Publisher ===')
+        publisher_id = self.get_publisher_id()
+        publisher = self.controller.find(publisher_id)
+        if publisher:
+            data = self.get_publisher_data()
+            self.controller.update(*data)
+        else:
+            print(self.__NOT_FOUND)
+        self.press_enter_to_continue()
+        self.clear_screen()
+
+    def delete(self) -> None:
+        print('\n=== Delete Publisher ===')
+        publisher_id = self.get_publisher_id()
+        publisher = self.controller.find(publisher_id)
+        if publisher:
+            self.controller.delete(publisher_id)
+        else:
+            print(self.__NOT_FOUND)
+        self.press_enter_to_continue()
+        self.clear_screen()
+
+    def get_publisher_data(self) -> Tuple[str, str, str, str]:
+        publisher_id = self.get_publisher_id()
+        legal_name = self.get_city()
+        city = self.get_city()
+        state = self.get_state()
+        return publisher_id, legal_name, city, state
+
+    @staticmethod
+    def get_publisher_id() -> str:
+        while True:
+            publisher_id = input('Publisher ID: ')
+            if PublisherValidator.validate_publisher_id(publisher_id):
+                return publisher_id
+            print("❌ Invalid publisher_id. Please enter an integer.")
+
+    @staticmethod
+    def get_legal_name() -> str:
+        while True:
+            legal_name = input('Legal Name: ')
+            if PublisherValidator.validate_legal_name(legal_name):
+                return legal_name
+            print("❌ Invalid legal name. Must be at least 10 characters.")
+
+    @staticmethod
+    def get_city() -> str:
+        while True:
+            city = input('City: ')
+            if PublisherValidator.validate_city(city):
+                return city
+            print("❌ Invalid city. Must be at least 10 characters.")
+
+    @staticmethod
+    def get_state() -> str:
+        while True:
+            state = input('State: ')
+            if PublisherValidator.validate_state(state):
+                return state
+            print("❌ Invalid state. Must be 2 characters.")
