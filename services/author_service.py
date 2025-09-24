30fb4a2bf78e5d304c5a7f141aba31194c1a7ae4
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-09-24 11:21:06 -0300

    add services para buscar objetos relacionados

---
+++ services/author_service.py
@@ -0,0 +1,20 @@
+from typing import List, Tuple
+from model.author import Author
+from model.author_dao import AuthorDao
+from model.exceptions import AuthorNotFoundError
+
+
+class AuthorService:
+
+    @staticmethod
+    def _get_all_authors() -> List[Author]:
+        """Load all authors from DAO (active and deleted)"""
+        return AuthorDao.load_all()
+
+    @staticmethod
+    def get_author_by_id(author_id: str) -> Author:
+        authors = AuthorService._get_all_authors()
+        author = next((a for a in authors if str(a.author_id) == author_id), None)
+        if author is None:
+            raise AuthorNotFoundError(f"Author with ID {author_id} not found.")
+        return author
---
+++ services/publisher_service.py
@@ -0,0 +1,20 @@
+from typing import List, Tuple
+from model.publisher import Publisher
+from model.publisher_dao import PublisherDao
+from model.exceptions import PublisherNotFoundError
+
+
+class PublisherService:
+
+    @staticmethod
+    def _get_all_publishers() -> List[Publisher]:
+        """Load all publishers from DAO (active and deleted)"""
+        return PublisherDao.load_all()
+
+    @staticmethod
+    def get_publisher_by_id(publisher_id: str) -> Publisher:
+        publishers = PublisherService._get_all_publishers()
+        publisher = next((p for p in publishers if str(p.publisher_id) == publisher_id), None)
+        if publisher is None:
+            raise PublisherNotFoundError(f"Publisher with ID {publisher_id} not found.")
+        return publisher
