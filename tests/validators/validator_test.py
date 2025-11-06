58fecb7af4f541e813bea8c080eb734002576a08
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-11-06 08:52:23 -0300

    WIP

--- tests/validators/validator_test.py
+++ tests/validators/validator_test.py
@@ -14,3 +14,24 @@ def test_not_empty_with_whitespace_string():
 def test_not_empty_with_empty_string():
     """Should return False when string is empty."""
     assert Validator.not_empty("") is False
+
+
+# TODO: test_min_length_valid_string():
+
+
+# TODO: test_min_length_invalid_string():
+
+
+# TODO: test_min_length_ignores_whitespace():
+
+
+# TODO: test_is_numeric_with_digits_only():
+
+
+# TODO: test_is_numeric_with_letters():
+
+
+# TODO: test_is_numeric_with_empty_string():
+
+
+# TODO: test_is_numeric_with_spaces():
