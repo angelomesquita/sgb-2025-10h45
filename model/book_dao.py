23a46b9f8a00d2d70b074e885e510712162f8f1b
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-12-04 08:37:19 -0300

    add book sqlite dao

--- model/book_dao.py
+++ model/book_dao.py
@@ -1,24 +1,116 @@
-from model.file_dao import FileDao
+from typing import Optional, Iterable
+
+from model.sqlite_dao import SqliteDao, T
 from model.book import Book
 from repository.author_repository import AuthorRepository
 from repository.publisher_repository import PublisherRepository
 
 
-class BookDao(FileDao[Book]):
-    _FILE_PATH = 'books.txt'
+class BookDao(SqliteDao[Book]):
+    @classmethod
+    def create_table(cls) -> None:
+        with cls._get_connection() as connection:
+            connection.execute(
+                """
+                CREATE TABLE IF NOT EXISTS books (
+                    isbn TEXT PRIMARY KEY,
+                    title TEXT NOT NULL,
+                    author_id TEXT NOT NULL,
+                    publisher_id TEXT NOT NULL,
+                    year INTEGER NOT NULL,
+                    quantity TEXT NOT NULL,
+                    deleted INTEGER NOT NULL DEFAULT 0,
+                    FOREIGN KEY (author_id) REFERENCES authors(author_id),
+                    FOREIGN KEY (publisher_id) REFERENCES publishers(publisher_id)
+                )
+                """
+            )
+            connection.commit()
 
-    @staticmethod
-    def _serialize(b: Book) -> str:
-        return f"{b.isbn}|{b.title}|{b.author.author_id}|{b.publisher.publisher_id}|{b.year}|{b.quantity}|{b.deleted}"
+    @classmethod
+    def save(cls, book: Book) -> None:
+        deleted_value = int(bool(book.deleted))
+        with cls._get_connection() as connection:
+            connection.execute(
+                """
+                INSERT INTO books (isbn, title, author_id, publisher_id, year, quantity, deleted)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                ON CONFLICT (isbn) DO UPDATE SET
+                    title = excluded.title,
+                    author_id = excluded.author_id,
+                    publisher_id = excluded.publisher_id,
+                    year = excluded.year,
+                    quantity = excluded.quantity,
+                    deleted = excluded.deleted
+                """,
+                (
+                    book.isbn,
+                    book.title,
+                    book.author.author_id,
+                    book.publisher.publisher_id,
+                    book.year,
+                    book.quantity,
+                    deleted_value
+                ),
+            )
+            connection.commit()
 
-    @staticmethod
-    def _deserialize(data: str) -> Book:
-        isbn, title, author_id, publisher_id, year, quantity, deleted = data.split("|")
+    @classmethod
+    def _build_book_from_row(cls, row) -> Book:
+        author_id = row['author_id']
+        publisher_id = row['publisher_id']
 
         author = AuthorRepository.get_author_by_id(author_id)
         publisher = PublisherRepository.get_publisher_by_id(publisher_id)
 
-        book = Book(isbn, title, author, publisher, year, quantity, deleted)
-        book.deleted = deleted.lower() == "true"
+        return Book(
+            isbn=row['isbn'],
+            title=row['title'],
+            author=author,
+            publisher=publisher,
+            year=row['year'],
+            quantity=row['quantity'],
+            deleted=bool(row['deleted'])
+        )
 
-        return book
+    @classmethod
+    def get_all(cls) -> Iterable[Book]:
+        with cls._get_connection() as connection:
+            rows = connection.execute(
+                """SELECT * FROM books WHERE deleted = 0"""
+            ).fetchall()
+
+        books = []
+        for row in rows:
+            book = cls._build_book_from_row(row)
+            books.append(book)
+
+        return books
+
+    @classmethod
+    def get_by_id(cls, isbn: str, deleted: int = 0) -> Optional[Book]:
+        with cls._get_connection() as connection:
+            row = connection.execute(
+                'SELECT * FROM books WHERE isbn = ? AND deleted = ?', (isbn, deleted, )
+            ).fetchone()
+
+        if row is None:
+            return None
+
+        return cls._build_book_from_row(row)
+
+    @classmethod
+    def delete(cls, isbn: str) -> None:
+        with cls._get_connection() as connection:
+            connection.execute(
+                'UPDATE books SET deleted = 1 WHERE isbn = ?', (isbn, )
+            )
+            connection.commit()
+
+    @classmethod
+    def restore(cls, isbn: str) -> None:
+        with cls._get_connection() as connection:
+            connection.execute(
+                'UPDATE books SET deleted = 0 WHERE isb = ?', (isbn, )
+            )
+            connection.commit()
--- start_database.py
+++ start_database.py
@@ -1,4 +1,5 @@
 from model.author_dao import AuthorDao
+from model.book_dao import BookDao
 from model.publisher_dao import PublisherDao
 
 
@@ -8,3 +9,6 @@ if __name__ == '__main__':
 
     PublisherDao.create_table()
     print('Publisher table created successfully.')
+
+    BookDao.create_table()
+    print('Book table created successfully.')
