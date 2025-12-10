7d7013cd08bb867f62e2dc3873df8ae53baa61d2
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-12-10 12:24:06 -0300

    ajustes e testes de bool_repository

--- repository/book_repository.py
+++ repository/book_repository.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Iterable, List, Optional
 from model.book import Book
 from model.book_dao import BookDao
 from model.exceptions import BookNotAvailableError, BookNotFoundError
@@ -7,15 +7,15 @@ from model.exceptions import BookNotAvailableError, BookNotFoundError
 class BookRepository:
 
     @staticmethod
-    def get_all_books() -> List[Book]:
-        return BookDao.load_all()
+    def get_all_books() -> Iterable[Book]:
+        return BookDao.get_all()
 
     @staticmethod
     def get_book_by_isbn(isbn: str) -> Book:
         books = BookRepository.get_all_books()
         book = next((b for b in books if str(b.isbn) == isbn), None)
         if book is None:
-            raise BookNotFoundError(f"book with isbn {isbn} not found")
+            raise BookNotFoundError(f"Book with isbn {isbn} not found.")
         return book
 
     @staticmethod
@@ -23,7 +23,7 @@ class BookRepository:
         title: Optional[str] = None,
         author: Optional[str] = None,
         available: Optional[bool] = None
-    ) -> List[Book]:
+    ) -> Iterable[Book]:
         """Search books applying optional filters"""
         results = BookRepository.get_all_books()
 
@@ -42,23 +42,26 @@ class BookRepository:
         return results
 
     @staticmethod
-    def decrease_quantity(book_isbn: str, amount: int = 1) -> None:
-        books = BookRepository.get_all_books()
-        for book in books:
-            quantity = int(book.quantity)
-            if book.isbn == book_isbn:
-                if quantity - amount < 0:
-                    raise BookNotAvailableError(f"Book '{book.title}' is out of stock.")
-                quantity -= amount
-                book.quantity = quantity
-                break
-        BookDao.save_all(books)
+    def decrease_quantity(isbn: str, amount: int = 1) -> Optional[bool]:
+        book = BookRepository.get_book_by_isbn(isbn)
+
+        if book is None:
+            raise BookNotFoundError(f"Book with isbn {isbn} not found.")
+
+        if (book.quantity - amount) < 0:
+            raise BookNotAvailableError(f"Book '{book.title}' is out of stock.")
+
+        book.quantity -= amount
+        BookDao.save(book)
+        return True
 
     @staticmethod
-    def increase_quantity(book_isbn: str, amount: int = 1) -> None:
-        books = BookRepository.get_all_books()
-        for book in books:
-            if book.isbn == book_isbn:
-                book.quantity += amount
-                break
-        BookDao.save_all(books)
+    def increase_quantity(isbn: str, amount: int = 1) -> Optional[bool]:
+        book = BookRepository.get_book_by_isbn(isbn)
+
+        if book is None:
+            raise BookNotFoundError(f"Book with isbn {isbn} not found.")
+
+        book.quantity += amount
+        BookDao.save(book)
+        return True
---
+++ tests/repository/book_repository_test.py
@@ -0,0 +1,109 @@
+import pytest
+
+from unittest.mock import patch
+from typing import Iterable
+from model.author import Author
+from model.book import Book
+from model.book_dao import BookDao
+from model.exceptions import BookNotAvailableError, BookNotFoundError
+from model.publisher import Publisher
+from repository.book_repository import BookRepository
+
+
+@pytest.fixture()
+def sample_books() -> Iterable[Book]:
+    """Fixture that provides a sample list of Books objects."""
+    author = Author(author_id="1", name="Author")
+    publisher = Publisher(publisher_id="1", legal_name="Publisher", city="City", state="ST")
+
+    return [
+        Book(isbn="9789789789781", title="Book 1", author=author, publisher=publisher, year=2025, quantity=1),
+        Book(isbn="9789789789782", title="Book 2", author=author, publisher=publisher, year=2025, quantity=1),
+        Book(isbn="9789789789783", title="Book 3", author=author, publisher=publisher, year=2025, quantity=1),
+    ]
+
+
+@pytest.mark.parametrize(
+    "isbn, expected_title, expected_author_name, expected_publisher_legal_name, expected_year, expected_quantity", [
+        ("9789789789781", "Book 1", "Author", "Publisher", 2025, 1),
+        ("9789789789782", "Book 2", "Author", "Publisher", 2025, 1),
+        ("9789789789783", "Book 3", "Author", "Publisher", 2025, 1),
+    ])
+def test_get_book_by_id_return_book(sample_books, isbn, expected_title, expected_author_name,
+                                    expected_publisher_legal_name, expected_year, expected_quantity):
+    """Ensures get_book_by_id() returns the correct Book when found."""
+    with patch.object(BookDao, 'get_all', return_value=sample_books) as mock_load_all:
+        book = BookRepository.get_book_by_isbn(isbn=isbn)
+
+        assert book.title == expected_title
+        assert book.author.name == expected_author_name
+        assert book.publisher.legal_name == expected_publisher_legal_name
+        assert book.year == expected_year
+        assert book.quantity == expected_quantity
+        mock_load_all.assert_called_once()
+
+
+def test_get_book_by_id_raises_error(sample_books):
+    """Ensures get_book_by_id() raise BookNotFound when isbn is not found."""
+    with patch.object(BookDao, 'get_all', return_value=sample_books) as mock_load_all:
+        with pytest.raises(BookNotFoundError, match="Book with isbn X999 not found."):
+            BookRepository.get_book_by_isbn(isbn="X999")
+        mock_load_all.assert_called_once()
+
+
+@pytest.mark.parametrize("search_by, value", [
+    ("title", "Book"),
+    ("author", "Author"),
+    ("available", True)
+])
+def test_search_book(sample_books, search_by, value):
+    """Ensures search() return the correct book when found."""
+    with patch.object(BookDao, 'get_all', return_value=sample_books) as mock_load_all:
+        filters = {search_by: value}
+        books = BookRepository.search(**filters)
+
+        assert books == sample_books
+        mock_load_all.assert_called_once()
+
+
+def test_search_book_return_empty_iterable_when_book_not_available(sample_books):
+    """Ensures search() return the empty list."""
+    with patch.object(BookDao, 'get_all', return_value=sample_books) as mock_load_all:
+        books = BookRepository.search(title="Book 1", author="Author", available=False)
+
+        assert books == []
+        mock_load_all.assert_called_once()
+
+
+def test_decrease_quantity_of_book(sample_books):
+    with patch.object(BookDao, 'get_all', return_value=sample_books) as mock_load_all:
+        result = BookRepository.decrease_quantity(isbn="9789789789781")
+
+        assert result
+        mock_load_all.assert_called_once()
+
+
+def test_increase_quantity_of_book(sample_books):
+    with patch.object(BookDao, 'get_all', return_value=sample_books) as mock_load_all:
+        result = BookRepository.increase_quantity(isbn="9789789789781")
+
+        assert result
+        mock_load_all.assert_called_once()
+
+
+def test_decrease_quantity_of_book_raises_error_when_book_not_found(sample_books):
+    with patch.object(BookDao, 'get_all', return_value=sample_books):
+        with pytest.raises(BookNotFoundError, match="Book with isbn X999 not found."):
+            BookRepository.decrease_quantity(isbn="X999")
+
+
+def test_decrease_quantity_of_book_raises_error_when_book_has_not_quantity(sample_books):
+    with patch.object(BookDao, 'get_all', return_value=sample_books):
+        with pytest.raises(BookNotAvailableError, match="Book 'Book 1' is out of stock."):
+            BookRepository.decrease_quantity(isbn="9789789789781", amount=2)
+
+
+def test_increase_quantity_of_book_raises_error_when_book_not_found(sample_books):
+    with patch.object(BookDao, 'get_all', return_value=sample_books):
+        with pytest.raises(BookNotFoundError, match="Book with isbn X999 not found."):
+            BookRepository.increase_quantity(isbn="X999")
